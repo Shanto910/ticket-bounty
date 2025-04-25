15ddb75faf67fc1b6458acc88e1f9b6091d6ace2
Shanto910 <shantoopaul@gmail.com>
2025-04-25 12:36:18 +0600

    add: scripts and sort package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
+import { FlatCompat } from '@eslint/eslintrc';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
+const compat = new FlatCompat({ baseDirectory: __dirname });
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		plugins: { 'simple-import-sort': simpleImportSort },
+		rules: {
+			'simple-import-sort/imports': [
+				'error',
+				{ groups: [['^\\u0000', '^node:', '^@?\\w', '^', '^\\.']] },
+			],
+			'simple-import-sort/exports': 'error',
+		},
+		languageOptions: { sourceType: 'module', ecmaVersion: 'latest' },
+	},
 ];
-
 export default eslintConfig;
