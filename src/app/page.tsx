029b32127e072246d8d16335f4c094e934157e75
Shanto910 <shantoopaul@gmail.com>
2025-04-25 15:02:27 +0600

    add: tailwindCSS styles and shadcnUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
+import { ticketsPath } from '@/paths';
+
 const HomePage = () => {
 	return (
 		<>
-			<h2 className="text-lg">Home Page</h2>
+			<div className="flex-1 flex flex-col gap-y-8">
+				<div>
+					<h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+					<p className="text-sm text-muted-foreground">Your home place to start</p>
+				</div>
+
+				<Separator />
+
+				<div className="flex-1 flex flex-col items-center">
+					<Link href={ticketsPath()} className="underline">
+						Go to Tickets Page
+					</Link>
+				</div>
+			</div>
 		</>
 	);
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,48 @@
+import { LucideCircleCheck, LucideFileText, LucidePencil } from 'lucide-react';
 import Link from 'next/link';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
 import { initialTickets } from '@/data';
 import { ticketPath } from '@/paths';
 
+const TICKET_ICONS = {
+	OPEN: <LucideFileText />,
+	IN_PROGRESS: <LucidePencil />,
+	DONE: <LucideCircleCheck />,
+};
+
 const TicketsPage = () => {
 	return (
-		<>
-			{initialTickets.map(ticket => (
-				<div key={ticket.id}>
-					<h2 className="text-lg">{ticket.title}</h2>
-					<Link href={ticketPath(ticket.id)} className="text-sm underline">
-						View
-					</Link>
-				</div>
-			))}
-		</>
+		<div className="flex-1 flex flex-col gap-y-8">
+			<div>
+				<h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+				<p className="text-sm text-muted-foreground">All your tickets at one place</p>
+			</div>
+
+			<Separator />
+
+			<div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
+				{initialTickets.map(ticket => (
+					<Card key={ticket.id} className="w-full max-w-[420px]">
+						<CardHeader>
+							<CardTitle className="flex gap-x-2 items-center">
+								<span>{TICKET_ICONS[ticket.status]}</span>
+								<h3 className="text-3xl tracking-tight truncate">{ticket.title}</h3>
+							</CardTitle>
+						</CardHeader>
+
+						<CardContent>
+							<p className="line-clamp-3 whitespace-break-spaces">{ticket.content}</p>
+						</CardContent>
+						<CardFooter>
+							<Link href={ticketPath(ticket.id)} className="text-sm underline">
+								View
+							</Link>
+						</CardFooter>
+					</Card>
+				))}
+			</div>
+		</div>
 	);
 };
 export default TicketsPage;
