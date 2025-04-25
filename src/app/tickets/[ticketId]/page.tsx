00ff2564d3138c64e0447d202ba10b827f5bf013
Shanto910 <shantoopaul@gmail.com>
2025-04-25 13:05:46 +0600

    add: dynamic ticket page content

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,3 +1,5 @@
+import { initialTickets } from '@/data';
+
 type TicketPageProps = {
 	params: {
 		ticketId: string;
@@ -5,9 +7,20 @@ type TicketPageProps = {
 };
 
 const TicketPage = ({ params }: TicketPageProps) => {
+	const ticket = initialTickets.find(ticket => ticket.id === params.ticketId);
+
+	if (!ticket) {
+		return (
+			<>
+				<h2 className="text-lg">Ticket not found</h2>
+			</>
+		);
+	}
+
 	return (
 		<>
-			<h2>Ticket Page {params.ticketId}</h2>
+			<h2 className="text-lg">{ticket.title}</h2>
+			<p className="text-sm">{ticket.content}</p>
 		</>
 	);
 };
