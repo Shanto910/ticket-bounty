23891088ec797bdf7ec2ddbf6e84032566f9e9b5
Shanto910 <shantoopaul@gmail.com>
2025-04-25 17:04:01 +0600

    change: extracted card component to ticket-item

---
+++ src/features/ticket/components/ticket-item.tsx
@@ -0,0 +1,39 @@
+import { LucideSquareArrowOutUpRight } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { ticketPath } from '@/paths';
+import { TICKET_ICONS } from '../constants';
+import { Ticket } from '../types';
+
+type TicketItemProps = { ticket: Ticket };
+
+const TicketItem = ({ ticket }: TicketItemProps) => {
+	const detailButton = (
+		<Button variant={'outline'} size={'icon'} asChild>
+			<Link href={ticketPath(ticket.id)}>
+				<LucideSquareArrowOutUpRight className="h-4 w-4" />
+			</Link>
+		</Button>
+	);
+
+	return (
+		<div className="w-full max-w-[420px] flex gap-x-1">
+			<Card className="w-full">
+				<CardHeader>
+					<CardTitle className="flex gap-x-2 items-center">
+						<span>{TICKET_ICONS[ticket.status]}</span>
+						<h3 className="text-3xl tracking-tight truncate">{ticket.title}</h3>
+					</CardTitle>
+				</CardHeader>
+
+				<CardContent>
+					<p className="line-clamp-3 whitespace-break-spaces">{ticket.content}</p>
+				</CardContent>
+			</Card>
+			<div className="flex flex-col gap-y-1">{detailButton}</div>
+		</div>
+	);
+};
+
+export default TicketItem;
