ba1a8a666af04a73603f67af480ddfa7c1a16b61
Shanto910 <shantoopaul@gmail.com>
2025-04-25 12:59:48 +0600

    add: data.js file and show in tickets page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+import { initialTickets } from '@/data';
+
 const TicketsPage = () => {
 	return (
 		<>
-			<h2 className="text-lg">Tickets Page</h2>
+			{initialTickets.map(ticket => (
+				<div key={ticket.id}>
+					<h2 className="text-lg">{ticket.title}</h2>
+					<Link href={`/tickets/${ticket.id}`} className="text-sm underline">
+						View
+					</Link>
+				</div>
+			))}
 		</>
 	);
 };
