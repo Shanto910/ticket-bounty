c0e6fbb5b8959ae60b5726918f20365bf51f348c
Shanto910 <shantoopaul@gmail.com>
2025-05-08 15:23:22 +0600

    DAY-3

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -3,13 +3,12 @@ import TicketItem from '@/features/ticket/components/ticket-item';
 import { getTicket } from '@/features/ticket/queries/get-ticket';
 
 type TicketPageProps = {
-	params: {
-		ticketId: string;
-	};
+	params: Promise<{ ticketId: string }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-	const ticket = await getTicket(params.ticketId);
+	const { ticketId } = await params;
+	const ticket = await getTicket(ticketId);
 
 	if (!ticket) {
 		notFound();
